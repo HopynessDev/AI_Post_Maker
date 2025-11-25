8938981c29aa1a89862fcf3eaddbaa84b4ffff65
HopynessDev <129218917+HopynessDev@users.noreply.github.com>
2025-11-25 15:35:52 +0400

    3rd commit

--- prisma/migrations/20251124114630_add_product_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN "description" TEXT;
-ALTER TABLE "Product" ADD COLUMN "imageUrl" TEXT;
-ALTER TABLE "Product" ADD COLUMN "price" TEXT;
-ALTER TABLE "Product" ADD COLUMN "title" TEXT;
--- prisma/migrations/20251124122946_add_more_product_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN "options" TEXT;
-ALTER TABLE "Product" ADD COLUMN "productType" TEXT;
-ALTER TABLE "Product" ADD COLUMN "variants" TEXT;
-ALTER TABLE "Product" ADD COLUMN "vendor" TEXT;
